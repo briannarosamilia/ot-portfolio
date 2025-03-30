d386ccd24eab173e29239dba0692e2f539e678eb
mikelanasa <mikelanasa@gmail.com>
2025-03-29 23:01:08 -0400

    mobile layout

--- src/Layout.vue
+++ src/Layout.vue
@@ -99,73 +99,75 @@ const items = [
         v-model="drawer"
         location="top"
         temporary
-        width="355"
+        width="105"
     >
       <v-list class="py-0" slim>
-        <v-list-item link prepend-icon="mdi-home-outline" title="Brianna Rosamilia, OTS" />
+        <v-list-item link title="Experience" @click="scrollTo('experience')" />
 
-        <v-list-group
-            prepend-icon="mdi-account-multiple-outline"
-            title="Customers"
-        >
-          <template #activator="{ props: activatorProps }">
-            <v-list-item v-bind="activatorProps" />
-          </template>
+        <v-list-item link title="Education" @click="scrollTo('education')" />
 
-          <v-list-item
-              link
-              prepend-icon="mdi-account-plus-outline"
-              title="Create New"
-          />
+<!--        <v-list-group-->
+<!--            prepend-icon="mdi-account-multiple-outline"-->
+<!--            title="Customers"-->
+<!--        >-->
+<!--          <template #activator="{ props: activatorProps }">-->
+<!--            <v-list-item v-bind="activatorProps" />-->
+<!--          </template>-->
 
-          <v-list-group prepend-icon="mdi-magnify" title="Search">
-            <template #activator="{ props: activatorProps }">
-              <v-list-item v-bind="activatorProps" />
-            </template>
+<!--          <v-list-item-->
+<!--              link-->
+<!--              prepend-icon="mdi-account-plus-outline"-->
+<!--              title="Create New"-->
+<!--          />-->
 
-            <v-list-item
-                link
-                prepend-icon="mdi-account-outline"
-                title="By Name"
-            />
+<!--          <v-list-group prepend-icon="mdi-magnify" title="Search">-->
+<!--            <template #activator="{ props: activatorProps }">-->
+<!--              <v-list-item v-bind="activatorProps" />-->
+<!--            </template>-->
 
-            <v-list-item
-                link
-                prepend-icon="mdi-email-outline"
-                title="By Email"
-            />
+<!--            <v-list-item-->
+<!--                link-->
+<!--                prepend-icon="mdi-account-outline"-->
+<!--                title="By Name"-->
+<!--            />-->
 
-            <v-list-item
-                link
-                prepend-icon="mdi-phone-outline"
-                title="By Phone"
-            />
-          </v-list-group>
-        </v-list-group>
+<!--            <v-list-item-->
+<!--                link-->
+<!--                prepend-icon="mdi-email-outline"-->
+<!--                title="By Email"-->
+<!--            />-->
 
-        <v-list-item link prepend-icon="mdi-calendar" title="Calendar" />
+<!--            <v-list-item-->
+<!--                link-->
+<!--                prepend-icon="mdi-phone-outline"-->
+<!--                title="By Phone"-->
+<!--            />-->
+<!--          </v-list-group>-->
+<!--        </v-list-group>-->
 
-        <v-list-item link prepend-icon="mdi-poll" title="Analytics" />
+<!--        <v-list-item link prepend-icon="mdi-calendar" title="Calendar" />-->
 
-        <v-divider />
+<!--        <v-list-item link prepend-icon="mdi-poll" title="Analytics" />-->
 
-        <v-list-item link prepend-icon="mdi-inbox-outline" title="Inbox" />
+<!--        <v-divider />-->
 
-        <v-list-item
-            link
-            prepend-icon="mdi-bell-outline"
-            title="Notifications"
-        />
+<!--        <v-list-item link prepend-icon="mdi-inbox-outline" title="Inbox" />-->
 
-        <v-divider />
+<!--        <v-list-item-->
+<!--            link-->
+<!--            prepend-icon="mdi-bell-outline"-->
+<!--            title="Notifications"-->
+<!--        />-->
 
-        <v-list-item
-            lines="two"
-            link
-            prepend-avatar="https://vuetifyjs.b-cdn.net/docs/images/avatars/planetary-subscriber.png"
-            subtitle="Vuetify Engineer"
-            title="John Leider"
-        />
+<!--        <v-divider />-->
+
+<!--        <v-list-item-->
+<!--            lines="two"-->
+<!--            link-->
+<!--            prepend-avatar="https://vuetifyjs.b-cdn.net/docs/images/avatars/planetary-subscriber.png"-->
+<!--            subtitle="Vuetify Engineer"-->
+<!--            title="John Leider"-->
+<!--        />-->
       </v-list>
     </v-navigation-drawer>
 
